52940b06319d2e059e893dea41aa9a2ccc26f18a
Carlos <carlos.alvarezdelavega@gmail.com>
2020-11-06 17:30:54 +0000

    fix navbar

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 // mobile menu
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
+const burger = document.getElementById('navbar-burger');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    document.getElementById(burger.dataset.target).classList.toggle('is-active');
 });
+
+// shadow scroll
+const navItems = document.getElementById('menubar').firstElementChild.children,
+    navSections = new Array(navItems.length);
+for (i = 0; i < navItems.length; i++)
+    navSections[i] = document.getElementById(navItems[i].dataset.target);
+
+const menuBarHeight = document.getElementById('menubar').offsetHeight;
+function isVisible(ele) {
+    const r = ele.getBoundingClientRect();
+    const h = (window.innerHeight || document.documentElement.clientHeight);
+    const w = (window.innerWidth || document.documentElement.clientWidth);
+    return (r.top <= h) && 
+        (r.top + r.height - menuBarHeight >= 0) && 
+        (r.left <= h) && 
+        (r.left + r.width >= 0);
+}
+function activateIfVisible() {
+    for (b = true, i = 0; i < navItems.length; i++) {
+        if (b && isVisible(navSections[i])) {
+            navItems[i].classList.add('is-active');
+            b = false;
+        } else 
+            navItems[i].classList.remove('is-active');
+    }
+}
+var isTicking = null;
+window.addEventListener('scroll', () => {
+    if (!isTicking) {
+        window.requestAnimationFrame(() => {
+            activateIfVisible();
+            isTicking = false;
+        });
+        isTicking = true;
+    }
+}, false);
+
+// smooth scrolling
+for (item of navItems) {
+    item.addEventListener('click', e => {
+        e.preventDefault();
+        window.scroll({ 
+            behavior: 'smooth', 
+            left: 0, 
+            top: document.getElementById(e.target.dataset.target).getBoundingClientRect().top + 
+                window.scrollY 
+        });
+    });
+}
